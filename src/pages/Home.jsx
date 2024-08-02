e9e66b9e2662fc2e0b7bae5d19a6d0d3c562b386
Raj Kumar Ballepu <108982663+rajkumarballepu@users.noreply.github.com>
2024-08-03 01:15:39 +0530

    axiosFix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios';
+import axios from 'redaxios';
 import { allMovies } from "../utils/APIroutes";
 import { useCookies } from 'react-cookie';
 import MovieCard from '../components/Movie/MovieCard';
--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useCookies } from 'react-cookie';
-import axios from 'axios';
+import axios from 'redaxios';
 import { allMovies } from '../utils/APIroutes';
 
 
